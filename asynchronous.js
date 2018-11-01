9d656f30080556626976ef0c7453195841f24636
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2018-11-01 10:19:24 +0700

    case 2

--- asynchronous.js
+++ asynchronous.js
@@ -1,9 +1,12 @@
 
 const a = () =>{
-	setTimeout(()=> {console.log("3 sec")},3000)
+	return new Promise ((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(console.log("3 sec"))
+		},3000)	
+	})
 }
 const b = () =>{
 	console.log("4 sec")
 }
-a()
-b()
+a().then(()=> b())
