18307f33e4e3ccaf34de9d6358ae83f808881e90
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2018-11-01 12:18:13 +0700

    add async await

--- asynchronous.js
+++ asynchronous.js
@@ -1,12 +1,14 @@
 
-const a = () =>{
-	return new Promise ((resolve,reject)=>{
-		setTimeout(()=>{
-			resolve(console.log("3 sec"))
-		},3000)	
-	})
+const a = async () =>{
+		await setTimeout(()=>{
+			console.log("3 sec")
+		},3000)
 }
-const b = () =>{
-	console.log("4 sec")
+const b = async () =>{
+		console.log("4 sec")
 }
-a().then(()=> b())
+const result = async () =>{
+	await a()
+	await b()
+}
+
